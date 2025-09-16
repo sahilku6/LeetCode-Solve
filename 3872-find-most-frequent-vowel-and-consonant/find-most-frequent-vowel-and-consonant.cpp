4c6b244bdc0ab8c876eeeb8a106bcbd297da4806
Sahil Kumar <166779000+sahilku6@users.noreply.github.com>
2025-09-16 08:00:58 +0530

    Time: 1 ms (45.91%) | Memory: 9.2 MB (82.96%) - LeetSync

--- 3872-find-most-frequent-vowel-and-consonant/find-most-frequent-vowel-and-consonant.cpp
+++ 3872-find-most-frequent-vowel-and-consonant/find-most-frequent-vowel-and-consonant.cpp
@@ -1,42 +1,17 @@
-#include <unordered_map>
-#include <queue>
-#include <string>
-#include <cctype>
-
-using namespace std;
-
 class Solution {
 public:
-    bool isVowel(char ch) {
-        ch = tolower(ch);
-        return ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u';
-    }
-
     int maxFreqSum(string s) {
-        unordered_map<char, int> mpV;
-        unordered_map<char, int> mpC;
-        priority_queue<int> pqVowel;
-        priority_queue<int> pqConso;
-
-        for (char ch : s) {
-            ch = tolower(ch);
-            if (isVowel(ch)) {
-                mpV[ch]++;
-            } else if (isalpha(ch)) {
-                mpC[ch]++;
+        int freq[26], maxVowel = 0, maxConso = 0;
+        for (char c : s) {
+            int i = c - 'a';
+            freq[i]++;
+            if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u') {
+                maxVowel = max(maxVowel, freq[i]);
+            }
+            else {
+                maxConso = max(maxConso, freq[i]);
             }
         }
-
-        for (auto [_, freq] : mpV) {
-            pqVowel.push(freq);
-        }
-        for (auto [_, freq] : mpC) {
-            pqConso.push(freq);
-        }
-
-        int maxVowel = pqVowel.empty() ? 0 : pqVowel.top();
-        int maxConso = pqConso.empty() ? 0 : pqConso.top();
-
         return maxVowel + maxConso;
     }
 };
