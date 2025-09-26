640911007c694e0358cf9b9c930e7a4764ce4e70
Sahil Kumar <166779000+sahilku6@users.noreply.github.com>
2025-09-26 17:37:19 +0530

    Time: 34 ms (45.52%) | Memory: 16.5 MB (95.74%) - LeetSync

--- 611-valid-triangle-number/valid-triangle-number.cpp
+++ 611-valid-triangle-number/valid-triangle-number.cpp
@@ -2,35 +2,20 @@ class Solution {
 public:
     int triangleNumber(vector<int>& nums) {
         int n = nums.size();
-        if (n < 3)
-            return 0;
-        sort(nums.begin(), nums.end());
         int count = 0;
-        for (int i = 0; i < n; i++) {
-            if (nums[i] == 0)
-                continue;
-            for (int j = i + 1; j < n; j++) {
-                int sum = nums[i] + nums[j];
-                int k = binary_search(nums, j + 1, n - 1, sum);
-                if (k != -1)
-                    count += (k - j);
+        sort(nums.begin(), nums.end());
+        for (int k = n - 1; k >= 2; k--) {
+            int i = 0;
+            int j = k - 1;
+            while (i < j) {
+                if (nums[i] + nums[j] > nums[k]) {
+                    count += (j - i);
+                    j--;
+                } else {
+                    i++;
+                }
             }
         }
         return count;
-    }
-
-private:
-    int binary_search(vector<int>& nums, int l, int r, int target) {
-        int k = -1;
-        while (l <= r) {
-            int mid = l + (r - l) / 2;
-            if (nums[mid] < target) {
-                k = mid;
-                l = mid + 1;
-            } else {
-                r = mid - 1;
-            }
-        }
-        return k;
     }
 };
