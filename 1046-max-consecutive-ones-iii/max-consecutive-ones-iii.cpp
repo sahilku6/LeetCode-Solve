1df2723d4b7c24f7e31a69b6bdf7297ffd0c42ac
Sahil Kumar <166779000+sahilku6@users.noreply.github.com>
2025-12-16 19:06:47 +0530

    Time: 3 ms (33.85%) | Memory: 69.7 MB (24.88%) - LeetSync

---
+++ 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int longestOnes(vector<int>& nums, int k) {
+        // think like to find a longest substring atmost zeros
+
+        int left = 0;
+        int right = 0;
+        int maxConsecutive = 0;
+        int noOfZeros = 0;
+        while (right < nums.size()) {
+
+            if (nums[right] == 0)
+                noOfZeros++;
+                
+            while (noOfZeros > k) {
+                if (nums[left] == 0) {
+                    noOfZeros--;
+                }
+                left++;
+            }
+
+            if (noOfZeros <= k) {
+                maxConsecutive = max(maxConsecutive, right - left + 1);
+            }
+            right++;
+        }
+        return maxConsecutive;
+    }
+};
